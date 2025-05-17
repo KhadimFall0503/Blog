dc71a2ff59ee5a09488d6218658ba08676a0cf90
Khadim Fall <khadimfall0503@gmail.com>
2025-05-17 13:53:30 +0000

    Changement

--- Mon_Blog/settings.py
+++ Mon_Blog/settings.py
@@ -14,9 +14,11 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-qg*aso*ta1+sxd6q5-agl!5*n+*q3uo$403sqr4t+z!!!5wo$g'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+# ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['blog-t2vd.onrender.com']
+
 
 
 # Application definition
